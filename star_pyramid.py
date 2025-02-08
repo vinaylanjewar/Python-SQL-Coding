0d56a6f637e4427257e0d9ac5ed50f1d0283596d
Vinay Lanjewar <96326032+vinaylanjewar@users.noreply.github.com>
2025-02-08 17:33:01 +0530

    Update star_pyramid.py

--- star_pyramid.py
+++ star_pyramid.py
@@ -1,3 +1,10 @@
+# Type 1 -->
+#      * 
+#     * * 
+#    * * * 
+#   * * * * 
+#  * * * * * 
+
 n = 5
 for i in range(n):
     for j in range(i,n):
@@ -6,3 +13,19 @@ for i in range(n):
         print("*",end =" ")
     
     print()
+
+## other pyramid type-->
+#    *
+#   ***
+#  *****
+# *******
+
+num = int(input('enter the num'))
+cnt = num//2
+# print(cnt)
+scnt = 1
+
+for i in range(cnt +1):
+    print(cnt*" " + "*"*scnt)
+    cnt = cnt-1
+    scnt = scnt + 2
