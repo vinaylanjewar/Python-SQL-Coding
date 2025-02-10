dbfa9a49a061c75452c6f0b9809eb7e2d620cc26
Vinay Lanjewar <96326032+vinaylanjewar@users.noreply.github.com>
2025-02-10 16:21:28 +0530

    Create Anagram.py

---
+++ Anagram.py
@@ -0,0 +1,12 @@
+a = "listen"
+b = "silent"
+
+x= sorted(a)
+y = sorted(b)
+
+if x==y:
+    print('its anagram')
+#     print(f'"{a}" and "{b}" are anagrams')
+else:
+    print('its not')
+#     print(f'"{a}" and "{b}" are not anagrams')
